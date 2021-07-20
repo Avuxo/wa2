e019b3518d3b2cd509ece66629c5c1f22b5a0e7d
ben <Avuxo@users.noreply.github.com>
2021-07-20 16:40:41 -0400

    add script to generate class definition from methods.json

---
+++ scripts/generate-class.js
@@ -0,0 +1,19 @@
+// script to generate class definition for direct3d class wrapper
+
+const { writeFileSync } = require('fs');
+const methods = require('./methods.json');
+
+// meant to be filled in later by hand;
+let result = 'class abc : public Abc {\r\n';
+
+for (method of methods) {
+    if (typeof method != 'object') {
+        result += `  ${method};\r\n`;
+    } else {
+        result += `  ${method.returnType} ${method.methodName}(${method.params});\r\n`;
+    }
+}
+
+result += '};'
+
+writeFileSync('class.h', result);
